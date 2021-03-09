8098ac23d9fe8ad963022418e3b67bd936af12d1
shreenivasm <59352181+shreenivasm@users.noreply.github.com>
2021-03-09 13:13:45 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel CI CD testing
                 </div>
 
                 <div class="links">
